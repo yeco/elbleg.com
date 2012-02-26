f298716762017b009eaa099df210e4e71f44a63f
Jasson Cascante <jcascante@gmail.com>
2012-02-25 23:43:41 -0600

    top links mods

---
+++ public/javascript/main.js
@@ -0,0 +1,28 @@
+var bleg = bleg || {};
+
+bleg = {
+	navBtns: function(argument) {
+		var nav_items = document.querySelectorAll('nav li');
+
+
+		for (i in nav_items) {
+			try {
+				nav_items[i].addEventListener("click", function() {
+					var goto = this.getElementsByTagName('a')[0].href;
+					window.location.href = goto;
+
+				}, false);
+
+			} catch (err) {}
+		}
+
+
+
+	},
+	init: function() {
+		this.navBtns();
+	}
+}
+
+
+bleg.init();
